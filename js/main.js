deff7a16257404fdd4a03029ab91ad153972754d
guadacastro <guadycasmar123@gmail.com>
2023-09-06 15:40:34 +0200

    added comments on the index.html and main.js files for better understanding

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
+// Obtinene el elemento input de texto por su ID
 const input = document.getElementById('input-task');
 // const taskList = document.getElementById('task-list');
 
-input.addEventListener("keyup", function (event) {
-    if (event.key === "Enter") {
+// Agrega un evento para escuchar la tecla 'Enter' y llamar a la funcion addTask()
+input.addEventListener('keyup', function (event) {
+    if (event.key === 'Enter') {
         addTask();
     }
 });
 
+
+// funcion para agregar una tarea 
 function addTask() {
     const textInput = input.value;
     // console.log(textInput);
+
+    // Verifica que si el campo de entrada esta vacia y si no esta vacia, llama a la siguiente funcion createTask()
     if (textInput === '') {
         alert('Write something first!');
     } else {
@@ -20,11 +26,14 @@ function addTask() {
 
 }
 
+//funcion para agregar una nueva tarea 
 function createTask(text) {
+    //obtiene el elemento de la lista de tareas pos su ID y el boton de selecionar todas las tareas. 
     const taskList = document.getElementById('task-list');
     const selectAllButton = document.getElementById('select-all-button');
-    selectAllButton.classList.remove('d-none');
+    selectAllButton.classList.remove('d-none'); // elimina la clase d-none para que se pueda ver el boton de selecionar todo
     
+    // crea elementos HTML para representar a la tarea
     const taskItem = document.createElement('div');
     taskItem.classList.add('task-item', 'row');
 
@@ -43,13 +52,14 @@ function createTask(text) {
 
 
  
-
+    // agrega los elementos al documento HTML
     taskRow.appendChild(icon1);
     taskRow.appendChild(p);
     taskRow.appendChild(icon2);
 
     taskItem.append(taskRow);
 
+    // agrega clases a los iconos para marcar/completar y eliminar tareas
     icon1.addEventListener('click', function () {
         if (icon1.classList.contains('fa-circle')){
             icon1.classList.replace('fa-circle', 'fa-circle-check');
@@ -73,7 +83,7 @@ function createTask(text) {
     // console.log(p)
     // console.log(icon1)
     // console.log(icon2)
-    console.log(taskItem)
+    // console.log(taskItem)
 
     taskList.appendChild(taskItem);
 
@@ -81,38 +91,46 @@ function createTask(text) {
 
 }
 
-
+//funcion para seleccionar/deseleccioanr todas las tareas 
+// esta funcion se llama cuando el usuario hace click en el boton select all
 function checkAll() {
-    const taskItems = document.querySelectorAll('.task-item');
-    const selectAllIcon = document.getElementById('select-all');
+    
+    const taskItems = document.querySelectorAll('.task-item'); //obtiene todos los elementos html con la clase 'task-item' (osea, las tareas individuales)
+    const selectAllIcon = document.getElementById('select-all'); //obtiene el icono select all por su ID
 
+    // si no hay tareas en la lista, no se hace nada y se sale de la funcnion
     if (taskItems.length === 0) {
         return;
     }
 
-    if (selectAllIcon.classList.contains("fa-circle")){
-        selectAllIcon.classList.replace('fa-circle', 'fa-circle-check');
+    // verifica el estado actual del icono select all
+    if (selectAllIcon.classList.contains('fa-circle')){
+        selectAllIcon.classList.replace('fa-circle', 'fa-circle-check'); // si el icono esta vacio, lo reemplaza por un circulo con marca de verificacion y le cambia el color
         selectAllIcon.classList.add('orange');
-    } else {
+    } else { // sino pasa lo contrario y reemplaza el icono por un circulo vacio
         selectAllIcon.classList.replace('fa-circle-check', 'fa-circle');
     }
 
+    // recorre todas las tareas individuales
     taskItems.forEach((taskItem) => {
-        const icon1 = taskItem.querySelector('.fa-regular');
-        const p = taskItem.querySelector('.task-text');
+        const icon1 = taskItem.querySelector('.fa-regular'); // obtiene el icono de circulo
+        const p = taskItem.querySelector('.task-text'); // obtiene el parrafo que contiene el texto de la tarea
 
+        // verifica el estado actual del icono de circulo
         if (icon1.classList.contains('fa-circle')) {
+            // si el icono es un circulo vacio, lo reemplaca con el circulo con marca de verificacion o tick y se cambia la apariencia del textp
             icon1.classList.replace('fa-circle', 'fa-circle-check');
-            p.classList.replace("incomplete", "completed");
-            p.classList.replace("black", "orange");
-            icon1.classList.replace("black", "orange");
+            p.classList.replace('incomplete', 'completed');
+            p.classList.replace('black', 'orange');
+            icon1.classList.replace('black', 'orange');
 
         } else {
+            // si el icono es un circulo con marca de verificacion, lo reemplaza con el otro icono y cambia la aparicencia del texto tambien 
             icon1.classList.replace('fa-circle-check', 'fa-circle');
-            p.classList.replace("completed", "incomplete");
-            p.classList.replace("orange", "black");
+            p.classList.replace('completed', 'incomplete');
+            p.classList.replace('orange', 'black');
             console.log(p)
-            icon1.classList.replace("orange", "black");
+            icon1.classList.replace('orange', 'black');
    
         }
 
