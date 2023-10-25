e9703aa45f7fb884eae0c2ef81eb7c077bdc7bb0
guadacastro <guadycasmar123@gmail.com>
2023-10-25 21:58:41 +0200

    added openweatherapp

--- js/inbox.js
+++ js/inbox.js
@@ -1,24 +1,60 @@
 
-// const dailyButton = document.getElementById('daily-button');
-// const weeklyButton = document.getElementById('weekly-button');
-// const monthlyButton = document.getElementById('monthly-button');
-// const yearButton = document.getElementById('year-button');
 
-// // weeklyButton.addEventListener('click',weeklyView);
-// // monthlyButton.addEventListener('click',monthlyView);
-// // yearButton.addEventListener('click',yearView);
+function getLocation() {
+    return new Promise(function (resolve, reject) {
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                resolve(position.coords);
+            }, function (error) {
+                reject(error);
+            });
+        } else {
+            reject('Geolocalizacion no esta disponible en tu navegador');
+        }
+    });
 
-// // document.getElementById('weekly-button').addEventListener('click', function() {
-// //     fetch('weekly.html')
-// //         .then(response => response.text())
-// //         .then(data => {
-// //             document.querySelector('main').innerHTML = data;
-// //         });
-// // })
+}
 
-// // fetch('header.html')
-// //     .then(response => response.text())
-// //     .then(headerContent => {
-// //         const headerElement = document.querySelector('header');
-// //         headerElement.innerHTML = headerContent;
-// //     });
+function getWeather(latitude, longitude) {
+    return new Promise(function (resolve, reject) {
+        const apiKey = 'ba0c6186592e59a1197ded18b1d68956';
+        const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey + '&units=metric';
+        console.log('LInk' + apiUrl);
+
+        $.get(apiUrl, function (data) {
+            resolve(data);            
+        }).fail(function (error) {
+            reject(error);
+        });
+    });
+
+}
+
+
+$(document).ready(function () {
+    getLocation()
+        .then(function (coords) {
+            return getWeather(coords.latitude, coords.longitude);
+        })
+        .then(function (data) {
+            const locationElement = document.getElementById('location');
+            const temperatureElement = document.getElementById('temperature');
+            // const descriptionElement = document.getElementById('description');
+            const iconElement = document.getElementById('weather-icon');
+            console.log(data)
+            
+            locationElement.textContent = data.name;
+            temperatureElement.textContent = Math.round(data.main.temp) + '°C';
+            // descriptionElement.textContent = data.weather[0].description;
+
+            const iconURL = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png';
+            iconElement.src = iconURL;
+
+            
+        })
+        .catch(function (error) {
+            const locationElement = document.getElementById('location'); 
+            locationElement.textContent = error;
+        })
+
+});
